b333520513f8aba98a5be19de8b873724b2efa7b
Lynn Luong <lynn.h.luong@gmail.com>
2022-06-24 10:51:03 -0400

    Phase 2: Sign up form done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -55,5 +55,20 @@ export const restoreUser = () => async dispatch => {
     dispatch(setUser(data.user));
     return response;
   };
-  
+
+  export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export default sessionReducer;
