a8adad0244ee5905dadf85992f69bf701a7603ba
Lynn Luong <lynn.h.luong@gmail.com>
2022-06-24 01:40:31 -0400

    Stuck on User Logout API Route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,14 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
