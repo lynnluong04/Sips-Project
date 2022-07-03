34ccc982fdd16de7613d11336402c11520ef6ed3
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-03 00:15:00 -0400

    update bug fixed

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -21,11 +21,30 @@ router.get('/:businessId', asyncHandler(async (req, res) => {
 }));
 
 router.put('/:businessId', asyncHandler(async (req, res) => {
-    const business = await Business.update(req.body, {
-      where: {id:req.body.id}
-    })
 
-    // await business.save();
+  // const business = await Business.update(req.body, {
+  //     where: {id:req.body.id}
+  //   })
+
+  const {name,
+    description,
+    address,
+    zipcode,
+    category,
+    phone,
+    websiteUrl} = req.body
+
+    const business = await Business.findByPk(req.params.businessId)
+    await business.update({
+      name,
+      description,
+      address,
+      zipcode,
+      category,
+      phone,
+      websiteUrl})
+
+      await business.save()
 
     return res.json(business)
 }));
